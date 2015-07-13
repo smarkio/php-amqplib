75f65f832fea43c44e98d4e7b2c86cc4b87c949c
Rui Campos <rui.campos@smark.io>
2015-07-13 16:54:21 +0100

    Heartbeat frames will decrease the timeout used when calling wait_channel - heartbeat frames do not reset the timeout

--- PhpAmqpLib/Connection/AbstractConnection.php
+++ PhpAmqpLib/Connection/AbstractConnection.php
@@ -564,11 +564,19 @@ class AbstractConnection extends AbstractChannel
      */
     protected function wait_channel($channel_id, $timeout = 0)
     {
+        // Keeping the original timeout unchanged.
+        $_timeout = $timeout;
         while (true) {
-            list($frame_type, $frame_channel, $payload) = $this->wait_frame($timeout);
+            $now = time();
+            list($frame_type, $frame_channel, $payload) = $this->wait_frame($_timeout);
 
             if ($frame_channel === 0 && $frame_type === 8) {
-                // skip heartbeat frames
+                // skip heartbeat frames and reduce the timeout by the time passed
+                if( $_timeout > 0 )
+                {
+                    $_timeout -= time() - $now;
+                    $_timeout = $_timeout > 0 ? $_timeout : 0;
+                }
                 continue;
 
             } else {
